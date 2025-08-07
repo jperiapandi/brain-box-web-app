0708e785fe238dedce93802bf19523baa555b85c
Periapandi Jeyaram <jperiapandi@gmail.com>
2025-08-07 16:17:53 +0530

    Update user profile

--- src/routes/pages/ProfilePage.tsx
+++ src/routes/pages/ProfilePage.tsx
@@ -1,8 +1,16 @@
 import type React from "react";
 import PageHeader from "../../components/headers/PageHeader";
-import { useContext, useEffect, useState, type MouseEventHandler } from "react";
+import {
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+  type ChangeEventHandler,
+  type KeyboardEventHandler,
+  type MouseEventHandler,
+} from "react";
 import { UserContext } from "../../contexts/UserContext";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, signOut, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router";
 import { HOME_PAGE_PATH } from "../router";
 import {
@@ -18,6 +26,11 @@ const ProfilePage: React.FunctionComponent = () => {
   const navigate = useNavigate();
   const user = useContext(UserContext);
   const [numOwnQuizzes, setNumOwnQuizzes] = useState(0);
+  const [updatedDisplayName, setUpdatedDisplayName] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [progress, setProgress] = useState(false);
+
+  const updatedDisplayNameField = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (user) {
@@ -42,28 +55,113 @@ const ProfilePage: React.FunctionComponent = () => {
     await signOut(getAuth());
     navigate(HOME_PAGE_PATH);
   };
+
+  const handleEditClick = () => {
+    setUpdatedDisplayName(user?.displayName ?? "");
+    setEditing(true);
+  };
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (evt) => {
+    setUpdatedDisplayName(evt.target.value);
+  };
+  const handleKeyBoardEvent: KeyboardEventHandler<HTMLInputElement> = (evt) => {
+    evt.stopPropagation();
+    // console.log(evt.key);
+
+    if (evt.key == "Enter") {
+      //TODO Save
+      saveDisplayName();
+      return;
+    }
+    if (evt.key == "Escape") {
+      //TODO Cancel
+      setEditing(false);
+      return;
+    }
+  };
+  const handleSaveClick = () => {
+    saveDisplayName();
+  };
+
+  const handleCancelClick = () => {
+    setEditing(false);
+  };
+
+  const saveDisplayName = async () => {
+    setProgress(true);
+
+    try {
+      if (user && updatedDisplayName != user.displayName) {
+        console.log(`Update Display Name ${updatedDisplayName}`);
+        await updateProfile(user, {
+          displayName: updatedDisplayName,
+        });
+        console.log(`Display Name updated!`);
+        setEditing(false);
+      } else {
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    setProgress(false);
+  };
+  
+  useEffect(() => {
+    if (editing === true) {
+      updatedDisplayNameField.current?.focus();
+    }
+  }, [editing]);
+
   return (
     <>
       <PageHeader title="Profile" navBack={true} profile={false} />
       {user ? (
-        <main className="page-content">
-          <div>
-            <h2>Hi,</h2>
-            <div>
-              <h1>{user.displayName}</h1>
-              <button className="icon-btn">
-                <span className="material-symbols-rounded">edit</span>
-              </button>
-            </div>
+        <main className="page-content profile-page">
+          <div className="welcome-content">
+            <div className="salute">Hi,</div>
+            {editing == false ? (
+              <div className="display-name-content">
+                <div className="display-name">{user.displayName}</div>
+                <button className="icon-btn" onClick={handleEditClick}>
+                  <span className="material-symbols-rounded">edit</span>
+                </button>
+              </div>
+            ) : (
+              <div className="display-name-editor-content">
+                <input
+                  type="text"
+                  value={updatedDisplayName}
+                  onChange={handleInputChange}
+                  onKeyUp={handleKeyBoardEvent}
+                  ref={updatedDisplayNameField}
+                  disabled={progress}
+                />
+                <button
+                  className="icon-btn"
+                  onClick={handleSaveClick}
+                  disabled={progress}
+                >
+                  <span className="material-symbols-rounded">check</span>
+                </button>
+                <button className="icon-btn" disabled={progress}>
+                  <span
+                    className="material-symbols-rounded"
+                    onClick={handleCancelClick}
+                  >
+                    close
+                  </span>
+                </button>
+              </div>
+            )}
           </div>
 
           {user.isAnonymous && (
-            <div>
-              <p>You are logged in as an Anonymous user.</p>
+            <div className="anonymous-content">
+              <span>You are an Anonymous user.</span>
             </div>
           )}
-          <h2>Number of quizzes created by you: {numOwnQuizzes}</h2>
-          <h2>Number of quizzes you attended: {0}</h2>
+          <p>Number of quizzes created by you: {numOwnQuizzes}</p>
+          <p>Number of quizzes you attended: {0}</p>
 
           <div>
             <button className="btn btn-logout" onClick={handleLogoutClick}>
